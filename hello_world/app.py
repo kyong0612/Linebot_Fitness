51038f7997b82c7c6ad156ebcfb0f1eb7114e78e
KIm <fenderst0612@gmail.com>
2021-02-11 15:44:25 +0900

    first commit

---
+++ hello_world/app.py
@@ -0,0 +1,16 @@
+import json
+
+# import requests
+
+
+def lambda_handler(event, context):
+
+    
+
+    return {
+        "statusCode": 200,
+        "body": json.dumps({
+            "message": "hello world",
+            # "location": ip.text.replace("\n", "")
+        }),
+    }
